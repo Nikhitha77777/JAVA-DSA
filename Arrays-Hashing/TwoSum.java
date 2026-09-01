a62d5f16ca872655886637de9000ac6f5e7c630d
Nikhitha Evuram <ganeshgaddadsa@gmail.com>
2026-09-01 17:09:19 +0530

    Update TwoSum.java

--- Arrays-Hashing/TwoSum.java
+++ Arrays-Hashing/TwoSum.java
@@ -1,6 +1,29 @@
 /*
 Problem: Two Sum
 Pattern: Arrays & Hashing
+*/
+
+/*
+Brute Force
+Time Complexity: O(n*n)
+Space Complexity: O(1)
+*/
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
+
+
+/*
+Optimal Solution
 Time Complexity: O(n)
 Space Complexity: O(n)
 */
